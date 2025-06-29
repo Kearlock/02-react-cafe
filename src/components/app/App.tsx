c6b45293a63ceaa01ce0ff8fe148c29280256a25
kearlock <kearlock@gmail.com>
2025-06-29 03:12:12 +0300

    did PascalCaseFileNames as robot wants

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import css from "./App.module.css";
 
-import CafeInfo from "../cafeinfo/CafeInfo.tsx";
-import VoteOptions from "../voteoptions/VoteOptions.tsx";
-import VoteStats from "../votestats/VoteStats.tsx";
-import Notification from "../notification/Notification.tsx";
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
+import VoteOptions from "../VoteOptions/VoteOptions.tsx";
+import VoteStats from "../VoteStats/VoteStats.tsx";
+import Notification from "../Notification/Notification.tsx";
 
 import type { Votes } from "../../types/votes.ts";
 import type { VoteType } from "../../types/votes.ts";
